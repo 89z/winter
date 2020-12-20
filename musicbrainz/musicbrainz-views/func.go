f57879df7bef1fe4bc6321ce43859952e0f947a1
Steven Penny <srpen6@gmail.com>
2020-12-20 11:22:25 -0600

    new file:   musicbrainz/musicbrainz-views/func.go

---
+++ musicbrainz/musicbrainz-views/func.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+   "net/http"
+   "net/url"
+   "os"
+   "path"
+   "strings"
+   "winter/musicbrainz"
+)
+
+func GetContents(s string) ([]byte, error) {
+   o, e := http.Get(s)
+   if e != nil {
+      return []byte{}, e
+   }
+   return ioutil.ReadAll(o.Body)
+}
+
+func FindSubmatch(pat string, sub []byte) []byte {
+   a := regexp.MustCompile(pat).FindSubmatch(sub)
+   if len(a) < 2 {
+      return []byte{}
+   }
+   return a[1]
+}
+
+func YoutubeResult(query_s string) (string, error) {
+   m := url.Values{}
+   m.Set("search_query", query_s)
+   res_s := "https://www.youtube.com/results?" + m.Encode()
+   println(res_s)
+   get_y, e := GetContents(res_s)
+   if e != nil {
+      return "", e
+   }
+   find_y := FindSubmatch("/vi/([^/]*)/", get_y)
+   return string(find_y), nil
+}
--- musicbrainz/musicbrainz-views/musicbrainz-views.go
+++ musicbrainz/musicbrainz-views/musicbrainz-views.go
@@ -3,70 +3,54 @@ package main
 import (
    "net/http"
    "net/url"
+   "os"
+   "path"
+   "strings"
+   "winter/musicbrainz"
 )
 
-func GetContents(s string) ([]byte, error) {
-   o, e := http.Get(s)
-   if e != nil {
-      return []byte{}, e
-   }
-   return ioutil.ReadAll(o.Body)
-}
-
-func FindSubmatch(pat string, sub []byte) []byte {
-   a := regexp.MustCompile(pat).FindSubmatch(sub)
-   if len(a) < 2 {
-      return []byte{}
-   }
-   return a[1]
-}
-
-func YoutubeResult(query_s string) (string, error) {
-   m := url.Values{}
-   m.Set("search_query", query_s)
-   res_s := "https://www.youtube.com/results?" + m.Encode()
-   println(res_s)
-   get_y, e := GetContents(res_s)
-   if e != nil {
-      return nil, e
-   }
-   find_y := FindSubmatch("/vi/([^/]*)/", get_y)
-   return string(find_y), nil
-}
-
 func main() {
-   if ($argc != 2) {
-      echo <<<eof
-usage:
-musicbrainz-views.php <URL>
+   if len(os.Args) != 2 {
+      println(`usage:
+musicbrainz-views <URL>
 
 examples:
 https://musicbrainz.org/release-group/d03bb6b1-d7b4-38ea-974e-847cbb31dca4
-https://musicbrainz.org/release/7a629d52-6a61-3ea1-a0a0-dd50bdef63b4
-eof;
-      exit(1);
+https://musicbrainz.org/release/7a629d52-6a61-3ea1-a0a0-dd50bdef63b4`)
+      os.Exit(1)
    }
-   $url_s = $argv[1];
-   $mbid_s = basename($url_s);
-   $dec_o = new MusicBrainzDecode($mbid_s);
-   if (str_contains($url_s, 'release-group')) {
-      $rel_a = $dec_o->group();
-      $rel_n = 0;
-      foreach ($rel_a as $idx_n => $cur_o) {
-         $rel_n = MusicBrainzReduce($rel_n, $cur_o, $idx_n, $rel_a);
+   url_s := os.Args[1]
+   mbid_s := path.Base(url_s)
+   mb_o := musicbrainz.New(mbid_s)
+   rel_m := assert.Map{}
+   if strings.Contains(url_s, "release-group") {
+      rel_a := mb_o.Group()
+      rel_n := 0
+      for idx_n := range rel_a {
+         cur_m := rel_a.M(idx_n)
+         rel_n = musicbrainz.Reduce(rel_n, cur_m, idx_n, rel_a)
       }
-      $rel_o = $rel_a[$rel_n];
-      echo 'musicbrainz.org/release/' . $rel_o->id, "\n";
+      rel_m = rel_a.M(rel_n)
+      print("musicbrainz.org/release/", rel_m.S("id"), "\n")
    } else {
-      $rel_o = $dec_o->release();
+      rel_m = mb_o.Release()
    }
-   foreach ($rel_o->{'artist-credit'} as $artist_o) {
-      $out_a[] = $artist_o->name;
+   out_a := assert.Slice{}
+   artist_a := rel_m.A("artist-credit")
+   for n := range artist_a {
+      artist_s := artist_a.M(n).S("name")
+      out_a = append(out_a, artist_s)
    }
-   $artists_s = implode(' ', $out_a);
-   foreach ($rel_o->media as $media_o) {
-      foreach ($media_o->tracks as $track_o) {
-         $id_s = yt_result($artists_s . ' ' . $track_o->title);
+   artist_s := strings.Join(out_a, " ")
+   media_a := rel_m.A("media")
+   for n := range media_a {
+      track_a := media_a.M(n).A("tracks")
+      for n := range track_a {
+         title_s := track_a.M(n).S("title")
+         id_s, e := YoutubeResult(artist_s + " " + title_s)
+         if e != nil {
+            log.Fatal(e)
+         }
          $info_o = youtube_info($id_s);
          echo youtube_views($info_o), "\n\n";
          usleep(500_000);
