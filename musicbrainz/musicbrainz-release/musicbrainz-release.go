9be89d94e06c941497f5d090355f0533f9fb2047
Steven Penny <srpen6@gmail.com>
2020-12-20 09:24:12 -0600

    deleted:    musicbrainz/reduce.go

--- musicbrainz/musicbrainz-release/musicbrainz-release.go
+++ musicbrainz/musicbrainz-release/musicbrainz-release.go
@@ -2,11 +2,12 @@ package main
 
 import (
    "log"
-   "musicbrainz/release"
    "os"
    "path"
    "strings"
    "time"
+   "winter/assert"
+   "winter/musicbrainz"
 )
 
 const (
@@ -26,22 +27,22 @@ https://musicbrainz.org/release-group/67898886-90bd-3c37-a407-432e3680e872`)
    }
    url_s := os.Args[1]
    mbid_s := path.Base(url_s)
-   dec_o := release.NewDecode(mbid_s)
-   rel_m := release.Map{}
+   mb_o := musicbrainz.New(mbid_s)
+   rel_m := assert.Map{}
    if strings.Contains(url_s, "release-group") {
-      rel_a, e := dec_o.Group()
+      rel_a, e := mb_o.Group()
       if e != nil {
          log.Fatal(e)
       }
       rel_n := 0
       for idx_n := range rel_a {
          cur_m := rel_a.M(idx_n)
-         rel_n = release.Reduce(rel_n, cur_m, idx_n, rel_a)
+         rel_n = musicbrainz.Reduce(rel_n, cur_m, idx_n, rel_a)
       }
       rel_m = rel_a.M(rel_n)
    } else {
-      e := error(nil)
-      rel_m, e = dec_o.Release()
+      var e error
+      rel_m, e = mb_o.Release()
       if e != nil {
          log.Fatal(e)
       }
--- musicbrainz/musicbrainz.go
+++ musicbrainz/musicbrainz.go
@@ -1,9 +1,10 @@
-package release
+package musicbrainz
 
 import (
    "encoding/json"
    "net/http"
    "net/url"
+   "winter/assert"
 )
 
 type MB struct {
@@ -12,7 +13,7 @@ type MB struct {
    Query url.Values
 }
 
-func NewDecode(mbid_s string) MB {
+func New(mbid_s string) MB {
    return MB{
       "https://musicbrainz.org/ws/2/release",
       mbid_s,
@@ -23,33 +24,80 @@ func NewDecode(mbid_s string) MB {
    }
 }
 
-func (mb_o MB) Group() (Slice, error) {
-   mb_o.Query["release-group"] = []string{mb_o.ID}
-   url_s := mb_o.API + "?" + mb_o.Query.Encode()
+func (this MB) Group() (assert.Slice, error) {
+   this.Query["release-group"] = []string{this.ID}
+   url_s := this.API + "?" + this.Query.Encode()
    println(url_s)
-   get_o, e := http.Get(url_s)
+   o, e := http.Get(url_s)
    if e != nil {
       return nil, e
    }
-   m := Map{}
-   e = json.NewDecoder(get_o.Body).Decode(&m)
+   m := assert.Map{}
+   e = json.NewDecoder(o.Body).Decode(&m)
    if e != nil {
       return nil, e
    }
    return m.A("releases"), nil
 }
 
-func (mb_o MB) Release() (Map, error) {
-   url_s := mb_o.API + "/" + mb_o.ID + "?" + mb_o.Query.Encode()
+func (this MB) Release() (assert.Map, error) {
+   url_s := this.API + "/" + this.ID + "?" + this.Query.Encode()
    println(url_s)
-   get_o, e := http.Get(url_s)
+   o, e := http.Get(url_s)
    if e != nil {
       return nil, e
    }
-   m := Map{}
-   e = json.NewDecoder(get_o.Body).Decode(&m)
+   m := assert.Map{}
+   e = json.NewDecoder(o.Body).Decode(&m)
    if e != nil {
       return nil, e
    }
    return m, nil
+}
+
+func Date(m assert.Map) string {
+   s := m.S("date")
+   switch len(s) {
+   case 0:
+      return "9999-12-31"
+   case 4:
+      return s + "-12-31"
+   case 6:
+      return s + "-31"
+   default:
+      return s
+   }
+}
+
+func IsOfficial(m assert.Map) bool {
+   return m.S("status") == "Official"
+}
+
+func TrackLen(m assert.Map) float64 {
+   var track_n float64
+   a := m.A("media")
+   for n := range a {
+      track_n += a.M(n).N("track-count")
+   }
+   return track_n
+}
+
+func Reduce(old_n int, new_m assert.Map, new_n int, old_a assert.Slice) int {
+   if new_n == 0 {
+      return 0
+   }
+   old_m := old_a.M(old_n)
+   if ! IsOfficial(new_m) {
+      return old_n
+   }
+   if Date(new_m) > Date(old_m) {
+      return old_n
+   }
+   if Date(new_m) < Date(old_m) {
+      return new_n
+   }
+   if TrackLen(new_m) >= TrackLen(old_m) {
+      return old_n
+   }
+   return new_n
 }
