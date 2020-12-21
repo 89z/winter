7d2198f3d86096ffdc09fa08009be93928f05196
Steven Penny <srpen6@gmail.com>
2020-12-20 18:15:51 -0600

    deleted:    musicbrainz/musicbrainz-release/insert.go

--- database/update.go
+++ database/update.go
@@ -8,61 +8,36 @@ import (
 )
 
 func CheckUpdate(open_o *sql.DB, artist_s, check_s string) error {
-   query_s := `
-   UPDATE artist_t SET check_s = ?
-   WHERE artist_n = ?
-   `
-   exec_o, e := open_o.Exec(query_s, check_s, artist_s)
-   if e != nil {
-      return fmt.Errorf("%v %v", exec_o, e)
-   }
-   return nil
+   _, e := open_o.Exec(
+      "update artist_t set check_s = ? where artist_n = ?", check_s, artist_s,
+   )
+   return e
 }
 
 func DateUpdate(open_o *sql.DB, album_s, date_s string) error {
-   query_s := `
-   UPDATE album_t SET date_s = ?
-   WHERE album_n = ?
-   `
-   exec_o, e := open_o.Exec(query_s, date_s, album_s)
-   if e != nil {
-      return fmt.Errorf("%v %v", exec_o, e)
-   }
-   return nil
+   _, e := open_o.Exec(
+      "update album_t set date_s = ? where album_n = ?", date_s, album_s,
+   )
+   return e
 }
 
 func NoteUpdate(open_o *sql.DB, song_s, note_s string) error {
-   query_s := `
-   UPDATE song_t SET note_s = ?
-   WHERE song_n = ?
-   `
-   exec_o, e := open_o.Exec(query_s, note_s, song_s)
-   if e != nil {
-      return fmt.Errorf("%v %v", exec_o, e)
-   }
-   return nil
+   _, e := open_o.Exec(
+      "update song_t set note_s = ? where song_n = ?", note_s, song_s,
+   )
+   return e
 }
 
 func PopUpdate(open_o *sql.DB, artist_s, pop_s string) error {
-   query_s := `
-   UPDATE artist_t SET pop_n = ?
-   WHERE artist_n = ?
-   `
-   exec_o, e := open_o.Exec(query_s, pop_s, artist_s)
-   if e != nil {
-      return fmt.Errorf("%v %v", exec_o, e)
-   }
-   return nil
+   _, e := open_o.Exec(
+      "update artist_t set pop_n = ? where artist_n = ?", pop_s, artist_s,
+   )
+   return e
 }
 
 func UrlUpdate(open_o *sql.DB, album_s, url_s string) error {
-   query_s := `
-   UPDATE album_t SET url_s = ?
-   WHERE album_n = ?
-   `
-   exec_o, e := open_o.Exec(query_s, url_s, album_s)
-   if e != nil {
-      return fmt.Errorf("%v %v", exec_o, e)
-   }
-   return nil
+   _, e := open_o.Exec(
+      "update album_t set url_s = ? where album_n = ?", query_s, url_s, album_s,
+   )
+   return e
 }
