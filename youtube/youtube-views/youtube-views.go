7648614e6c25bb3e1a0223bc175ca2b998edb19a
Steven Penny <srpen6@gmail.com>
2021-01-24 13:31:39 -0600

    deleted:    youtube/youtube-views/main.go

--- youtube/youtube-views/youtube-views.go
+++ youtube/youtube-views/youtube-views.go
@@ -1,8 +1,25 @@
 package main
-import "log"
 
-func check(e error) {
-   if e != nil {
-      log.Fatal(e)
+import (
+   "github.com/89z/x"
+   "net/url"
+   "os"
+   "winter/youtube"
+)
+
+func main() {
+   if len(os.Args) != 2 {
+      println("youtube-views <URL>")
+      os.Exit(1)
    }
+   url_s := os.Args[1]
+   o, e := url.Parse(url_s)
+   check(e)
+   id_s := o.Query().Get("v")
+   info, e := youtube.Info(id_s)
+   check(e)
+   view_n, e := youtube.Views(info)
+   check(e)
+   color, _ := youtube.Color(view_n)
+   println(color)
 }
