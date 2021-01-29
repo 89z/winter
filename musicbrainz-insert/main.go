5ff93ae0c2adad0600df4cfce89d4af7d6079125
Steven Penny <srpen6@gmail.com>
2021-01-29 07:31:32 -0600

    +	github.com/89z/x v1.5.1

--- musicbrainz-insert/main.go
+++ musicbrainz-insert/main.go
@@ -6,7 +6,6 @@ import (
    "github.com/89z/winter"
    "github.com/89z/x"
    "github.com/89z/x/musicbrainz"
-   "log"
    "os"
    _ "github.com/mattn/go-sqlite3"
 )
@@ -20,66 +19,58 @@ https://musicbrainz.org/release/7cc21f46-16b4-4479-844c-e779572ca834
 https://musicbrainz.org/release-group/67898886-90bd-3c37-a407-432e3680e872`)
       os.Exit(1)
    }
-   album, e := musicbrainz.Release(os.Args[1])
-   x.Check(e)
-   album_s := album.S("title")
-   date_s := album.S("date")
-   winter_s := os.Getenv("WINTER")
-   db, e := sql.Open("sqlite3", winter_s)
+   db, e := sql.Open(
+      "sqlite3", os.Getenv("WINTER"),
+   )
    x.Check(e)
    tx, e := db.Begin()
    x.Check(e)
+   album, e := musicbrainz.Release(os.Args[1])
+   x.Check(e)
    // ALBUM
-   album_n, e := winter.Insert(
+   albumId, e := winter.Insert(
       tx,
       "album_t (album_s, date_s, url_s) values (?, ?, '')",
-      album_s,
-      date_s,
+      album.Title,
+      album.Date,
    )
    x.Check(e)
-   var (
-      artist_n int
-      artists []int
-      songs []song
-   )
    // CREATE ARTIST ARRAY
-   credit := album.A("artist-credit")
-   for n := range credit {
-      // Chicago, Chicago Transit Authority
-      name := credit.M(n).M("artist").S("name")
+   var (
+      artist int
+      artists []int
+   )
+   for _, credit := range album.ArtistCredit {
       query := tx.QueryRow(
-         "select artist_n from artist_t where artist_s = ?", name,
+         // Chicago, Chicago Transit Authority
+         "select artist_n from artist_t where artist_s = ?", credit.Artist.Name,
       )
-      e = query.Scan(&artist_n)
-      x.Check(e, name)
-      artists = append(artists, artist_n)
+      e = query.Scan(&artist)
+      x.Check(e, credit.Artist.Name)
+      artists = append(artists, artist)
    }
    // CREATE SONG ARRAY
-   media := album.A("media")
-   for n := range media {
-      track_a := media.M(n).A("tracks")
-      for n := range track_a {
-         track_m := track_a.M(n)
-         song_s := track_m.S("title")
-         note_s := note(track_m)
-         songs = append(songs, song{song_s, note_s})
+   var songs []titleNote
+   for _, media := range album.Media {
+      for _, track := range media.Tracks {
+         songs = append(songs, titleNote{
+            track.Title, note(track.Length),
+         })
       }
    }
    // ITERATE SONG ARRAY
-   for _, song_o := range songs {
-      song_n, e := winter.Insert(
+   for _, each := range songs {
+      song, e := winter.Insert(
          tx,
          "song_t (song_s, note_s, album_n) values (?, ?, ?)",
-         song_o.title,
-         song_o.note,
-         album_n,
+         each.title,
+         each.note,
+         albumId,
       )
       x.Check(e)
       // ITERATE ARTIST ARRAY
-      for _, artist_n := range artists {
-         _, e = winter.Insert(
-            tx, "song_artist_t values (?, ?)", song_n, artist_n,
-         )
+      for _, artist := range artists {
+         _, e = winter.Insert(tx, "song_artist_t values (?, ?)", song, artist)
          x.Check(e)
       }
    }
--- musicbrainz-insert/musicbrainz-insert.go
+++ musicbrainz-insert/musicbrainz-insert.go
@@ -1,25 +1,21 @@
 package main
+import "time"
 
-import (
-   "github.com/89z/x"
-   "time"
-)
-
-func note(m x.Map) string {
-   if m["length"] == nil {
+func note(length int) string {
+   if length == 0 {
       return "?:??"
    }
-   n := time.Duration(m.N("length")) * time.Millisecond
-   if n < 179_500 * time.Millisecond {
+   dur := time.Duration(length) * time.Millisecond
+   if dur < 179_500 * time.Millisecond {
       return "short"
    }
-   if n > 15 * time.Minute {
+   if dur > 15 * time.Minute {
       return "long"
    }
    return ""
 }
 
-type song struct {
+type titleNote struct{
    title string
    note string
 }
