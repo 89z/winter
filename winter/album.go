18536b526b761eca6803bd39683f784a20c9ebdf
Steven Penny <srpen6@gmail.com>
2021-02-06 21:29:30 -0600

    +   query, e := tx.Query(

--- winter/album.go
+++ winter/album.go
@@ -22,15 +22,15 @@ func copyAlbum(tx *sql.Tx, source , dest string) error {
       return e
    }
    // COPY NOTES
-   query_o, e := tx.Query(
+   query, e := tx.Query(
       "select song_s, note_s from song_t where album_n = ?", source,
    )
    if e != nil {
       return e
    }
    songs := map[string]string{}
-   for query_o.Next() {
-      e = query_o.Scan(&song, &note)
+   for query.Next() {
+      e = query.Scan(&song, &note)
       if e != nil {
          return e
       }
@@ -49,32 +49,33 @@ func copyAlbum(tx *sql.Tx, source , dest string) error {
    return nil
 }
 
-func deleteAlbum(tx *sql.Tx, album_s string) error {
-   query_o, e := tx.Query(
-      "select song_n from song_t where album_n = ?", album_s,
-   )
+func deleteAlbum(tx *sql.Tx, album string) error {
+   query, e := tx.Query("select song_n from song_t where album_n = ?", album)
    if e != nil {
       return e
    }
-   song_n, song_a := 0, []int{}
-   for query_o.Next() {
-      e = query_o.Scan(&song_n)
+   var (
+      song int
+      songs []int
+   )
+   for query.Next() {
+      e = query.Scan(&song)
       if e != nil {
          return e
       }
-      song_a = append(song_a, song_n)
+      songs = append(songs, song)
    }
-   for _, song_n := range song_a {
-      e = winter.Delete(tx, "song_t where song_n = ?", song_n)
+   for _, song := range songs {
+      e = winter.Delete(tx, "song_t where song_n = ?", song)
       if e != nil {
          return e
       }
-      e = winter.Delete(tx, "song_artist_t where song_n = ?", song_n)
+      e = winter.Delete(tx, "song_artist_t where song_n = ?", song)
       if e != nil {
          return e
       }
    }
-   e = winter.Delete(tx, "album_t where album_n = ?", album_s)
+   e = winter.Delete(tx, "album_t where album_n = ?", album)
    if e != nil {
       return e
    }
