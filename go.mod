055c5a346c3796ae13222963321f60802ecc5353
Steven Penny <srpen6@gmail.com>
2021-04-05 07:46:12 -0500

    +	github.com/89z/x v1.11.8

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module winter
 go 1.16
 
 require (
-	github.com/89z/x v1.11.6
+	github.com/89z/x v1.11.8
 	github.com/mattn/go-sqlite3 v1.14.6
 )
--- winter/less.go
+++ winter/less.go
@@ -9,9 +9,7 @@ import (
 func less() (*exec.Cmd, io.WriteCloser, error) {
    cmd := exec.Command("less")
    pipe, e := cmd.StdinPipe()
-   if e != nil {
-      return nil, nil, e
-   }
+   if e != nil { return nil, nil, e }
    cmd.Stdout = os.Stdout
    return cmd, pipe, cmd.Start()
 }
