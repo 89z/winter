fa869c386e647da675d10f3fe68d391a7e533684
Steven Penny <srpen6@gmail.com>
2021-05-15 13:00:57 -0500

    +   db, err := sql.Open("sqlite3", file)

--- winter.go
+++ winter.go
@@ -11,34 +11,34 @@ type Rows = sql.Rows
 type Tx struct { *sql.Tx }
 
 func NewTx(file string) (Tx, error) {
-   db, e := sql.Open("sqlite3", file)
-   if e != nil {
-      return Tx{}, e
+   db, err := sql.Open("sqlite3", file)
+   if err != nil {
+      return Tx{}, err
    }
-   tx, e := db.Begin()
-   if e != nil {
-      return Tx{}, e
+   tx, err := db.Begin()
+   if err != nil {
+      return Tx{}, err
    }
    return Tx{tx}, nil
 }
 
 func (tx Tx) Delete(query string, arg ...interface{}) error {
    fmt.Println(arg)
-   _, e := tx.Exec("delete from " + query, arg...)
-   return e
+   _, err := tx.Exec("delete from " + query, arg...)
+   return err
 }
 
 func (tx Tx) Insert(query string, arg ...interface{}) (int64, error) {
    fmt.Println(arg)
-   res, e := tx.Exec("insert into " + query, arg...)
-   if e != nil {
-      return 0, e
+   res, err := tx.Exec("insert into " + query, arg...)
+   if err != nil {
+      return 0, err
    }
    return res.LastInsertId()
 }
 
 func (tx Tx) Update(query string, arg ...interface{}) error {
    fmt.Println(arg)
-   _, e := tx.Exec("update " + query, arg...)
-   return e
+   _, err := tx.Exec("update " + query, arg...)
+   return err
 }
